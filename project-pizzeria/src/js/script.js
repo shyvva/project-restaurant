ffe6d1ac2efefb50907fde5e516615af13311ae2
Dawid Przydryga <shivva90@gmail.com>
2020-03-24 20:06:52 +0100

    Add product menu: function renderInMenu, and init Accordion

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -52,14 +52,90 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+
+      console.log('new Product', thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generateHTML = templates.menuProduct(thisProduct.data);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generateHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: click event listener to trigger */
+      clickTrigger.addEventListener('click', function () {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        /* find all active products */
+        const productsActive = document.querySelectorAll(classNames.menuProduct.wrapperActive);
+        /* START LOOP: for each active product */
+        for (let productActive of productsActive) {
+          /* START: if the active product isn't the element of thisProduct */
+          if (productActive != thisProduct.element) {
+          /* remove class active for the active product */
+            productActive.classList.remove('active');
+          /* END: if the active product isn't the element of thisProduct */
+          }
+          /* END LOOP: for each active product */
+        }
+        /* END: click event listener to trigger */
+        console.log('clicked');
+      });
+    }
+  }
+
+
+
   const app = {
-    init: function(){
+    initMenu: function () {
+      const thisApp = this;
+
+      console.log('thisApp.data', thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
